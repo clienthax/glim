bc64b21a0d5c694a1ca077a23368ea0409dde307
z3y <z3y@protonmail.com>
2026-05-14 01:22:03 +0200

    fix light probes temporarly

--- stilb/src/sh.rs
+++ stilb/src/sh.rs
@@ -1,4 +1,4 @@
-use std::f32;
+use std::f32::{self, consts::PI};
 
 use crate::math::Vector3;
 
@@ -18,17 +18,14 @@ pub struct SHProbe {
     pub position_z: f32,
 }
 
-const SH_L0_NORMALIZATION: f32 = 0.2820947917738781434740397257803862929220253146644994284220428608;
-const SH_L1_NORMALIZATION: f32 = 0.4886025119029199215863846228383470045758856081942277021382431574;
-
 impl SHProbe {
     #[inline]
     pub fn normalize(&mut self, samples: u32) {
         let inv_samples = 1.0 / (samples as f32);
 
-        self.l0 = self.l0 * (inv_samples * SH_L0_NORMALIZATION);
+        self.l0 = self.l0 * (0.5 * (1.0 / PI).sqrt()) * PI * inv_samples;
 
-        let l1_scale = inv_samples * SH_L1_NORMALIZATION;
+        let l1_scale = inv_samples * (0.5 * (3.0 / PI).sqrt()) * (2.0 * PI / 3.0);
         self.l1x = self.l1x * l1_scale;
         self.l1y = self.l1y * l1_scale;
         self.l1z = self.l1z * l1_scale;
